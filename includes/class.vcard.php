7bfb071a0f2004949b3c2105a911c3b8d459fed3
shazahm1@hotmail.com <shazahm1@hotmail.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-24 02:36:17 +0000

    git-svn-id: https://plugins.svn.wordpress.org/connections/trunk@97101 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/class.vcard.php
+++ includes/class.vcard.php
@@ -142,12 +142,17 @@ class vCard extends entry
   
 	public function download()
 	{
-		if (!$this->filename) { $this->filename = trim($this->data['display_name']); }
-		$this->filename = str_replace(" ", "_", $this->filename);
+		$filename = urlencode(serialize($this->filename));
+		$card = urlencode(serialize($this->card));
+		
+		return '<a href="http://www.sandbox.nh-online.com/wp-content/plugins/connections/includes/download.vCard.php?filename=' . $filename . '&card=' . $card . '">Add to Address Book</a>';
+		
+		//if (!$this->filename) { $this->filename = trim($this->data['display_name']); }
+		//$this->filename = str_replace(" ", "_", $this->filename);
 			//header("Content-type: text/directory");
 			//header("Content-Disposition: attachment; filename=" . $this->filename . ".vcf");
 			//header("Pragma: public");
-			echo $this->card;
-		return true;
+			//echo $this->card;
+		//return true;
 	}
 }
--- includes/download.vCard.php
+++ includes/download.vCard.php
@@ -1,12 +1,17 @@
 <?php
-    public function download()
-	{
-		if (!$this->filename) { $this->filename = trim($this->data['display_name']); }
-		$this->filename = str_replace(" ", "_", $this->filename);
+    //public function download()
+	//{
+		$filename = $_GET['filename'];
+		$card = $_GET['card'];
+		
+		if (!$filename) { $filename = trim($data['display_name']); }
+		$filename = str_replace(" ", "_", $filename);
 			header("Content-type: text/directory");
-			header("Content-Disposition: attachment; filename=" . $this->filename . ".vcf");
+			header("Content-Disposition: attachment; filename=" . $filename . ".vcf");
 			header("Pragma: public");
-			echo $this->card;
-		return true;
-	}
+			//echo $card;
+			//print_r($_GET['filename']);
+			print_r($_GET['card']);
+		//return true;
+	//}
 ?>
