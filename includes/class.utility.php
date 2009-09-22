86988b66d99c594239c19198a245b5cfcea4c16a
shazahm1@hotmail.com <shazahm1@hotmail.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-22 00:43:58 +0000

    Rodelle's Salon Suites
    git-svn-id: https://plugins.svn.wordpress.org/connections/tags/RodellesSalonProfessionals@156848 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/class.utility.php
@@ -0,0 +1,29 @@
+<?php
+
+class cnCounter {
+     private $step;
+     private $count; 
+
+     public function getcount() {
+          return $this->count;
+     }
+ 
+     public function getstep() {
+          return $this->step;
+     }
+
+     public function changestep($newval) {
+          if(is_integer($newval))
+          $this->step = $newval;
+     }
+
+     public function step() {
+          $this->count += $this->step;
+     }
+
+     public function reset() {
+          $this->count = 0;
+          $this->step = 1;
+     }
+}
+?>
