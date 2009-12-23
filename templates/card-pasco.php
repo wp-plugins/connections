0885557cd28f6971e688865223351eece824e785
shazahm1@hotmail.com <shazahm1@hotmail.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-23 04:24:56 +0000

    git-svn-id: https://plugins.svn.wordpress.org/connections/trunk@186377 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/card-pasco.php
+++ templates/card-pasco.php
@@ -1,20 +1,18 @@
 <div id="entry_id_<?php echo $entry->getId(); ?>" class="cn-entry" style="-moz-border-radius:4px; background-color:#FFFFFF; border:1px solid #E3E3E3; margin:8px 0px; padding:6px; position: relative; width: 523px;">
 	<table width="100%" border="0px" bgcolor="#FFFFFF" bordercolor="#E3E3E3" cellspacing="0px" cellpadding="0px">
 	    <tr>
-	        <td align="left" width="50%" valign="top">
-	        	<?php echo $entry->getCardImage() ?>
-				
-				<div style="clear:both; margin: 0 5px;">
+	        <td align="left" width="55%" valign="top">
+	        	<div style="clear:both; margin: 0 5px;">
 					<div style="margin-bottom: 10px;">
-						<span style="font-size:larger;font-variant: small-caps"><strong><?php echo $entry->getFullFirstLastNameBlock() ?></strong></span><br />
+						<span style="font-size:larger;font-variant: small-caps; margin-bottom: 10px;"><strong><?php echo $entry->getFullFirstLastNameBlock() ?></strong></span><br />
 						
 						<?php echo $entry->getTitleBlock() ?>
 						<?php echo $entry->getOrgUnitBlock() ?>
+						
+						<?php if ($entry->getImageLinked() && $entry->getImageDisplay()) echo '<img class="photo" alt="Photo of ' . $entry->getFirstName() . ' ' . $entry->getLastName() . '" style="-moz-border-radius:4px; background-color: #FFFFFF; border:1px solid #E3E3E3; margin: 10px 0 10px 0; padding:5px;" src="' . CN_IMAGE_BASE_URL . $entry->getImageNameCard() . '" />' . "\n"; ?>
 					</div>
 					
 					<?php
-						echo $entry->getAddressBlock();
-						
 						if ($entry->getAddresses())
 						{
 							$addressObject = new cnAddresses;
@@ -22,7 +20,7 @@
 							{
 								$map_link = "http://maps.google.com/?q=";
 								
-								if ($entry->getOrganization() != NULL) $map_link .= $entry->getOrganization() . '+';
+								//if ($entry->getOrganization() != NULL) $map_link .= $entry->getOrganization() . '+';
 								//if ($addressObject->getLineOne($addressRow) != null) $map_link .= $addressObject->getLineOne($addressRow) . '+';
 								if ($addressObject->getLineTwo($addressRow) != null) $map_link .= $addressObject->getLineTwo($addressRow) . '+';
 								if ($addressObject->getCity($addressRow) != null) $map_link .= $addressObject->getCity($addressRow) . '+';
@@ -56,65 +54,89 @@
 				            $iframe_google_map = $map_link . $map_parms;
 						}
 						
-						$data = '<div class="google_map" style="background-color: #E3E3E3; display: none;"><iframe width="523" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="' . $iframe_google_map . '"></iframe></div>';
+						$data = '<div id="map_' . $entry->getId() . '" style="background-color: #E3E3E3; display: none; margin-top: 10px;"><iframe width="523" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="' . $iframe_google_map . '"></iframe></div>';
 					?>
 				</div>
 	        </td>
 	        <td align="right" valign="top">
 	        	<div style="clear:both; margin: 5px 5px;">
-		        	<?php echo $entry->getConnectionGroupBlock() ?>
-					
-					<?php echo $entry->getPhoneNumberBlock() ?>
+		        	<?php echo $entry->getPhoneNumberBlock() ?>
+					<?php echo $entry->getAddressBlock(); ?>
 					<?php echo $entry->getEmailAddressBlock() ?>
-					
 					<?php echo $entry->getImBlock() ?>
-					<?php //echo $entry->getWebsiteBlock() ?>
 					
-					<?php
-					$websiteObject = new cnWebsite;
-						if ($entry->getWebsites())
-						{
-							$anchorOut .= '<div class="websites" style="margin-bottom: 10px;">' . "\n";
-							foreach ($entry->getWebsites() as $websiteRow)
+					<div class="websites" style="margin-bottom: 10px;">
+					 
+						<?php
+						$websiteObject = new cnWebsite;
+							if ($entry->getWebsites())
 							{
-								if ($websiteObject->getAddress($websiteRow) != null) $anchorOut .= '<a class="url" href="' . $websiteObject->getAddress($websiteRow) . '">Visit their website.</a>' . "\n";
+								foreach ($entry->getWebsites() as $websiteRow)
+								{
+									if ($websiteObject->getAddress($websiteRow) != null) $anchorOut .= '<a class="url" href="' . $websiteObject->getAddress($websiteRow) . '" target="_blank">Visit Website</a>' . "\n";
+								}
 							}
-							$anchorOut .= "</div>" . "\n";
-						}
-						echo $anchorOut;
-					?>
+						?>
+					</div>
 					
-					<?php echo $entry->getBirthdayBlock('F j') ?>
-					<?php echo $entry->getAnniversaryBlock() ?>
 				</div>
 	        </td>
 	    </tr>
 	    
+	    <tr>
+	        <td valign="bottom">
+	        	<?php echo $vCard->download() ?><?php if (!empty($anchorOut)) echo ' | ' . $anchorOut; ?>
+	        </td>
+			<td align="right" valign="bottom">
+				
+				<?php if ($entry->getNotes() != '') { ?>
+				
+				<a href="#" id="close_description_link_<?php echo $entry->getId(); ?>" onclick='jQuery("a#close_description_link_<?php echo $entry->getId(); ?>").live("click", function(e){
+				jQuery("a#close_description_link_<?php echo $entry->getId(); ?>").hide();
+				jQuery("#description_block_<?php echo $entry->getId(); ?>").fadeOut();
+				jQuery("a#description_link_<?php echo $entry->getId(); ?>").fadeIn();
+				}); return false' style="display: none;">Close Description</a>
+				
+				<a href="#" id="description_link_<?php echo $entry->getId(); ?>" onclick='jQuery("a#description_link_<?php echo $entry->getId(); ?>").live("click", function(e){
+				jQuery("a#description_link_<?php echo $entry->getId(); ?>").hide();
+				jQuery("#description_block_<?php echo $entry->getId(); ?>").fadeIn();
+				jQuery("a#close_description_link_<?php echo $entry->getId(); ?>").fadeIn();
+				}); return false'>Business Description</a> | 
+				
+				<?php } ?>
+				
+				<a href="#" id="close_map_link_<?php echo $entry->getId(); ?>" class="close_map_link" onclick='jQuery("a#close_map_link_<?php echo $entry->getId(); ?>").live("click", function(e){
+				var divH = jQuery("#entry_id_<?php echo $entry->getId(); ?>").height() - 400;
+				jQuery("#map_<?php echo $entry->getId(); ?>").fadeOut();
+				//jQuery("#entry_id_<?php echo $entry->getId(); ?>").animate({height: divH}, 500);
+				jQuery("a#close_map_link_<?php echo $entry->getId(); ?>").hide();
+				jQuery("a#map_link_<?php echo $entry->getId(); ?>").fadeIn();
+				jQuery("#map_<?php echo $entry->getId(); ?>").remove();
+				}); return false' style="display: none;">Close Map</a>
+				
+				<a href="#" id="map_link_<?php echo $entry->getId(); ?>" class="open_map_link" onclick='jQuery("a#map_link_<?php echo $entry->getId(); ?>").live("click", function(e){
+        		var $this = jQuery(this);
+				var data = $this.attr("data");
+				var divH = jQuery("#entry_id_<?php echo $entry->getId(); ?>").height() + 400;
+				//jQuery("#entry_id_<?php echo $entry->getId(); ?>").animate({height: divH}, 1000);
+				jQuery(data).appendTo(jQuery("#entry_id_<?php echo $entry->getId(); ?>"));
+				jQuery("#map_<?php echo $entry->getId(); ?>").fadeIn(2000);
+				jQuery("a#map_link_<?php echo $entry->getId(); ?>").hide();
+				jQuery("a#close_map_link_<?php echo $entry->getId(); ?>").fadeIn();
+				}); return false' data='<?php echo $data; ?>'>Show Map</a>
+	        </td>
+	    </tr>
+		
 		<tr>
 			<td colspan="2">
 				<?php 
 					if ($entry->getNotes() != '')
 					{
-						echo '<div style="margin-bottom: 10px;">' . $entry->getNotesBlock() . '</div>';
+						echo '<div id="description_block_' . $entry->getId() . '" style="display: none; margin: 10px 0 0 0;">' . $entry->getNotesBlock() . '</div>';
 					}
 				?>
 			</td>
 		</tr>
-		
-	    <tr>
-	        <td valign="bottom">
-	        	<?php echo $vCard->download() ?>
-	        </td>
-			<td align="right" valign="bottom">
-				<a href="#" id="map_link_<?php echo $entry->getId(); ?>" class="map_link" onclick='jQuery("a#map_link_<?php echo $entry->getId(); ?>").live("click", function(e){
-        		var $this = jQuery(this);
-		        var data = $this.attr("title");
-				jQuery(data).appendTo(jQuery("#entry_id_<?php echo $entry->getId(); ?>"));
-				jQuery(".google_map").fadeIn(6000);
-				jQuery("a#map_link_<?php echo $entry->getId(); ?>").remove();
-				}); return false' title='<?php echo $data; ?>'>Show Map</a>
-	        </td>
-	    </tr>
 		
 	</table>
 	<?php
