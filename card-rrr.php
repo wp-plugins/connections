70e3c60592366d4a1f775ced5951d2a1822c724a
shazahm1@hotmail.com <shazahm1@hotmail.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-15 02:25:00 +0000

    git-svn-id: https://plugins.svn.wordpress.org/connections/branches/templates@229426 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- card-rrr.php
+++ card-rrr.php
@@ -4,16 +4,69 @@
 	        <td align="left" width="55%" valign="top" style="vertical-align: top;">
 	        	<div style="clear:both; margin: 0 5px;">
 					<div style="margin-bottom: 10px;">
-						<span style="font-size:larger;font-variant: small-caps"><strong><?php echo $entry->getFullFirstLastNameBlock() ?></strong></span><br />
+						<span style="font-size:larger;font-variant: small-caps; margin-bottom: 10px;"><strong><?php echo $entry->getFullFirstLastNameBlock() ?></strong></span><br />
 						
 						<?php echo $entry->getTitleBlock() ?>
-						<?php
-							if ($entry->getOrganization() || $entry->getDepartment()) echo '<div class="org">' . "\n";
-							if ($entry->getOrganization() && $entry->getEntryType() != 'organization') echo '<span class="organization-name">' . $entry->getOrganization() . '</span><br />' . "\n";
-							if ($entry->getDepartment()) echo '<strong><span class="organization-unit">' . $entry->getDepartment() . '</span></strong><br />' . "\n";
-							if ($entry->getOrganization() || $entry->getDepartment()) echo '</div>' . "\n";
-						?>
+						<?php echo $entry->getOrgUnitBlock() ?>
+						
+						<?php if ($entry->getImageLinked() && $entry->getImageDisplay()) echo '<img class="photo" alt="Photo of ' . $entry->getFirstName() . ' ' . $entry->getLastName() . '" style="-moz-border-radius:4px; background-color: #FFFFFF; border:1px solid #E3E3E3; margin: 10px 0 10px 0; padding:5px;" src="' . CN_IMAGE_BASE_URL . $entry->getImageNameCard() . '" />' . "\n"; ?>
 					</div>
+					
+				</div>
+	        </td>
+	        <td align="right" valign="top" style="text-align: right;">
+	        	
+				<div style="clear:both; margin: 5px 5px;">
+		        	
+					<?php
+						if ($entry->getPhoneNumbers())
+						{
+							$phoneNumberObject = new cnPhoneNumber();
+							echo '<div class="phone_numbers" style="margin-bottom: 10px;">' . "\n";
+							foreach ($entry->getPhoneNumbers() as $phoneNumberRow) 
+							{
+								if ($phoneNumberObject->getNumber($phoneNumberRow) != null)
+								{
+									switch ($phoneNumberRow['type'])
+									{
+										case 'home':
+											$phoneNumberName = 'Home Phone';
+											break;
+										case 'homephone':
+											$phoneNumberName = 'Home Phone';
+											break;
+										case 'homefax':
+											$phoneNumberName = 'Home Fax';
+											break;
+										case 'cell':
+											$phoneNumberName = 'Cell Phone';
+											break;
+										case 'cellphone':
+											$phoneNumberName = 'Cell Phone';
+											break;
+										case 'work':
+											$phoneNumberName = 'Phone';
+											break;
+										case 'workphone':
+											$phoneNumberName = 'Phone';
+											break;
+										case 'workfax':
+											$phoneNumberName = 'Fax';
+											break;
+										case 'fax':
+											$phoneNumberName = 'Fax';
+											break;
+									}
+									
+									//Type for hCard compatibility. Hidden.
+									echo  '<strong>' . $phoneNumberName . '</strong>: <span class="tel">' . $entry->gethCardTelType($phoneNumberRow) . '<span class="value">' .  $phoneNumberObject->getNumber($phoneNumberRow) . '</span></span><br />' . "\n";
+								}
+							}
+							echo '</div>' . "\n";
+						}
+					?>
+					
+					<?php echo $entry->getEmailAddressBlock() ?>
 					
 					<?php echo $entry->getAddressBlock() ?>
 					
@@ -65,17 +118,6 @@
 						
 					?>
 					
-				</div>
-	        </td>
-	        <td align="right" valign="top" style="text-align: right;">
-	        	<?php echo $entry->getCardImage() ?>
-				
-				<div style="clear:both; margin: 5px 5px;">
-		        	<?php echo $entry->getConnectionGroupBlock() ?>
-					
-					<?php echo $entry->getPhoneNumberBlock() ?>
-					<?php echo $entry->getEmailAddressBlock() ?>
-					
 					<?php echo $entry->getImBlock() ?>
 					<?php if (method_exists($entry, 'getSocialMediaBlock')) echo $entry->getSocialMediaBlock() ?>
 					
@@ -87,7 +129,21 @@
 	    
 	    <tr>
 	        <td valign="bottom">
-	        	<?php echo $vCard->download() ?>
+	        	<?php if ($entry->getNotes() != '') { ?>
+				
+				<a href="#" id="close_note_link_<?php echo $entry->getId(); ?>" onclick='jQuery("a#close_note_link_<?php echo $entry->getId(); ?>").live("click", function(e){
+				jQuery("a#close_note_link_<?php echo $entry->getId(); ?>").hide();
+				jQuery("#note_block_<?php echo $entry->getId(); ?>").fadeOut();
+				jQuery("a#note_link_<?php echo $entry->getId(); ?>").fadeIn();
+				}); return false' style="display: none;">Hide Green Points</a>
+				
+				<a href="#" id="note_link_<?php echo $entry->getId(); ?>" onclick='jQuery("a#note_link_<?php echo $entry->getId(); ?>").live("click", function(e){
+				jQuery("a#note_link_<?php echo $entry->getId(); ?>").hide();
+				jQuery("#note_block_<?php echo $entry->getId(); ?>").fadeIn();
+				jQuery("a#close_note_link_<?php echo $entry->getId(); ?>").fadeIn();
+				}); return false'>Show Green Points</a>
+				
+				<?php } ?>
 				
 				<?php if ($entry->getWebsites()) echo ' | '; ?>
 				
@@ -106,40 +162,21 @@
 	        </td>
 			<td align="right" valign="bottom"  style="text-align: right;">
 				
-				<?php if ($entry->getNotes() != '') { ?>
-				
-				<a href="#" id="close_note_link_<?php echo $entry->getId(); ?>" onclick='jQuery("a#close_note_link_<?php echo $entry->getId(); ?>").live("click", function(e){
-				jQuery("a#close_note_link_<?php echo $entry->getId(); ?>").hide();
-				jQuery("#note_block_<?php echo $entry->getId(); ?>").fadeOut();
-				jQuery("a#note_link_<?php echo $entry->getId(); ?>").fadeIn();
-				}); return false' style="display: none;">Hide Coupon</a>
-				
-				<a href="#" id="note_link_<?php echo $entry->getId(); ?>" onclick='jQuery("a#note_link_<?php echo $entry->getId(); ?>").live("click", function(e){
-				jQuery("a#note_link_<?php echo $entry->getId(); ?>").hide();
-				jQuery("#note_block_<?php echo $entry->getId(); ?>").fadeIn();
-				jQuery("a#close_note_link_<?php echo $entry->getId(); ?>").fadeIn();
-				}); return false'>Show Coupon</a>
-				
-				<?php
-					} 
-					if ($entry->getBio() != '' && $entry->getNotes() != '') echo ' | ';
-				?>
 				<?php if ($entry->getBio() != '') { ?>
 				
 				<a href="#" id="close_bio_link_<?php echo $entry->getId(); ?>" onclick='jQuery("a#close_bio_link_<?php echo $entry->getId(); ?>").live("click", function(e){
 				jQuery("a#close_bio_link_<?php echo $entry->getId(); ?>").hide();
 				jQuery("#bio_block_<?php echo $entry->getId(); ?>").fadeOut();
 				jQuery("a#bio_link_<?php echo $entry->getId(); ?>").fadeIn();
-				}); return false' style="display: none;">Hide Bio</a>
+				}); return false' style="display: none;">Close Description</a>
 				
 				<a href="#" id="bio_link_<?php echo $entry->getId(); ?>" onclick='jQuery("a#bio_link_<?php echo $entry->getId(); ?>").live("click", function(e){
 				jQuery("a#bio_link_<?php echo $entry->getId(); ?>").hide();
 				jQuery("#bio_block_<?php echo $entry->getId(); ?>").fadeIn();
 				jQuery("a#close_bio_link_<?php echo $entry->getId(); ?>").fadeIn();
-				}); return false'>Show Bio</a>
+				}); return false'>Business Description</a>
 				
 				<?php } ?>
-				
 				<?php if ($entry->getBio() != '' && $data != '') echo ' | '; ?>
 				
 				<?php if ($data != '') { ?>
@@ -177,12 +214,12 @@
 				<?php 
 					if ($entry->getNotes() != '')
 					{
-						echo '<div id="note_block_' . $entry->getId() . '" style="display: none; margin: 10px 0 0 0;"><strong>Coupon</strong><br />' . $entry->getNotesBlock() . '</div>';
+						echo '<div id="note_block_' . $entry->getId() . '" style="display: none; margin: 10px 0 0 0;"><strong>Green Points</strong><br />' . $entry->getNotesBlock() . '</div>';
 					}
 					
 					if ($entry->getBio() != '')
 					{
-						echo '<div id="bio_block_' . $entry->getId() . '" style="display: none; margin: 10px 0 0 0;"><strong>Biography</strong><br />' . $entry->getBioBlock() . '</div>';
+						echo '<div id="bio_block_' . $entry->getId() . '" style="display: none; margin: 10px 0 0 0;"><strong>Business Description</strong><br />' . $entry->getBioBlock() . '</div>';
 					}
 				?>
 			</td>
@@ -192,5 +229,6 @@
 	<?php
 		unset($anchorOut);
 		unset($data);
+		unset($showMap);
 	?>
 </div>
