9f42cbc00fba9c3f96ed0f5194160095558f20da
shazahm1@hotmail.com <shazahm1@hotmail.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-20 02:25:54 +0000

    git-svn-id: https://plugins.svn.wordpress.org/connections/branches/templates@264727 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- clarkcountylive.php
+++ clarkcountylive.php
@@ -2,19 +2,60 @@
 	<table width="100%" border="0px" bgcolor="#FFFFFF" bordercolor="#E3E3E3" cellspacing="0px" cellpadding="0px" style="width: 100%">
 	    <tr>
 	        <td align="left" width="55%" valign="top" style="vertical-align: top;">
-	        	<div style="clear:both; margin: 0 5px;">
+	        	
+				<div style="clear:both; margin: 0 5px;">
 					<div style="margin-bottom: 10px;">
 						<span style="font-size:larger;font-variant: small-caps; margin-bottom: 10px;"><strong><?php echo $entry->getFullFirstLastNameBlock() ?></strong></span><br />
 						
 						<?php echo $entry->getTitleBlock() ?>
 						<?php echo $entry->getOrgUnitBlock() ?>
 						
-						<?php if ($entry->getImageLinked() && $entry->getImageDisplay()) echo '<img class="photo" alt="Photo of ' . $entry->getFirstName() . ' ' . $entry->getLastName() . '" style="-moz-border-radius:4px; background-color: #FFFFFF; border:1px solid #E3E3E3; margin: 10px 0 10px 0; padding:5px;" src="' . CN_IMAGE_BASE_URL . $entry->getImageNameCard() . '" />' . "\n"; ?>
 					</div>
-					
 				</div>
+				
 	        </td>
 	        <td align="right" valign="top" style="text-align: right;">
+	        	
+				<div style="clear:both; margin: 5px 5px;">
+		        	
+					<span style="display: block; font-style: italic; margin-bottom: 8px;"><?php $entry->getCategoryBlock( array('separator' => ', ', 'before' => '<span>', 'after' => '</span>', 'label' => '') ); ?></span>
+					
+				</div>
+	
+	        </td>
+	    </tr>
+	    
+		<tr>
+	        <td align="left" width="55%" valign="top" colspan="2" style="vertical-align: top;">
+	        	
+				<div style="float:left; margin: 0px 10px 10px 0px;">
+					<?php if ($entry->getImageLinked() && $entry->getImageDisplay()) echo '<img class="photo" alt="Photo of ' . $entry->getFirstName() . ' ' . $entry->getLastName() . '" style="-moz-border-radius:4px; background-color: #FFFFFF; border:1px solid #E3E3E3; margin: 10px 0 10px 0; padding:5px;" src="' . CN_IMAGE_BASE_URL . $entry->getImageNameCard() . '" />' . "\n"; ?>
+				</div>
+				
+				<div style="margin: 10px 10px 10px;">
+		        	<?php
+						if ( mb_strlen($entry->getBio()) > 550 )
+						{
+							echo mb_substr($entry->getBio(), 0, 550) , '...';
+						}
+						else
+						{
+							echo $entry->getBio();
+						}
+					?>
+				</div>
+				
+	        </td>
+	    </tr>
+		
+		<tr>
+			<td align="left" width="55%" valign="top" style="vertical-align: top;">
+	        	<div style="clear:both; margin: 0 5px;">
+					<?php echo $entry->getAddressBlock() ?>
+				</div>
+	        </td>
+			
+			<td align="right" valign="top" style="text-align: right;">
 	        	
 				<div style="clear:both; margin: 5px 5px;">
 		        	
@@ -65,9 +106,9 @@
 						}
 					?>
 					
-					<?php echo $entry->getEmailAddressBlock() ?>
+					<?php echo $entry->getWebsiteBlock() ?>
 					
-					<?php echo $entry->getAddressBlock() ?>
+					<?php echo $entry->getEmailAddressBlock() ?>
 					
 					<?php
 						// Build the address query for Google.
@@ -122,87 +163,86 @@
 					<?php echo $entry->getBirthdayBlock('F j') ?>
 					<?php echo $entry->getAnniversaryBlock() ?>
 				</div>
-	        </td>
-	    </tr>
-	    
+	        </td>		
+		</tr>
+		
 	    <tr>
 	        <td valign="bottom">
+	        	
+				<?php if ($data != '') { ?>
+				
+					<a href="#" id="close_map_link_<?php echo $entry->getId(); ?>" class="close_map_link" onclick='jQuery("a#close_map_link_<?php echo $entry->getId(); ?>").live("click", function(e){
+					var divH = jQuery("#entry_id_<?php echo $entry->getId(); ?>").height() - 400;
+					jQuery("#map_<?php echo $entry->getId(); ?>").fadeOut();
+					//jQuery("#entry_id_<?php echo $entry->getId(); ?>").animate({height: divH}, 500);
+					jQuery("a#close_map_link_<?php echo $entry->getId(); ?>").hide();
+					jQuery("a#map_link_<?php echo $entry->getId(); ?>").fadeIn();
+					jQuery("#map_<?php echo $entry->getId(); ?>").remove();
+					}); return false' style="display: none;">Close Map</a>
+					
+					<a href="#" id="map_link_<?php echo $entry->getId(); ?>" class="open_map_link" onclick='jQuery("a#map_link_<?php echo $entry->getId(); ?>").live("click", function(e){
+	        		var $this = jQuery(this);
+					var data = $this.attr("data");
+					var divH = jQuery("#entry_id_<?php echo $entry->getId(); ?>").height() + 400;
+					//jQuery("#entry_id_<?php echo $entry->getId(); ?>").animate({height: divH}, 1000);
+					jQuery(data).appendTo(jQuery("#entry_id_<?php echo $entry->getId(); ?>"));
+					jQuery("#map_<?php echo $entry->getId(); ?>").fadeIn(2000);
+					jQuery("a#map_link_<?php echo $entry->getId(); ?>").hide();
+					jQuery("a#close_map_link_<?php echo $entry->getId(); ?>").fadeIn();
+					}); return false' data='<?php echo $data; ?>'>Show Map</a>
+					
+					<?php echo ' | '; ?>
+				
+				<?php } ?>
+				
 	        	<?php echo $vCard->download() ?>
 				
-				<?php if ($entry->getWebsites()) echo ' | '; ?>
+	        </td>
+			
+			<td align="right" valign="bottom"  style="text-align: right;">
+				
+				<?php 
+					if ($entry->getBio() != '')
+					{
+						if ( mb_strlen($entry->getBio()) > 550 )
+						{
+				?>
+				
+					<a href="#" id="close_bio_link_<?php echo $entry->getId(); ?>" onclick='jQuery("a#close_bio_link_<?php echo $entry->getId(); ?>").live("click", function(e){
+					jQuery("a#close_bio_link_<?php echo $entry->getId(); ?>").hide();
+					jQuery("#bio_block_<?php echo $entry->getId(); ?>").fadeOut();
+					jQuery("a#bio_link_<?php echo $entry->getId(); ?>").fadeIn();
+					}); return false' style="display: none;">Close Description</a>
+					
+					<a href="#" id="bio_link_<?php echo $entry->getId(); ?>" onclick='jQuery("a#bio_link_<?php echo $entry->getId(); ?>").live("click", function(e){
+					jQuery("a#bio_link_<?php echo $entry->getId(); ?>").hide();
+					jQuery("#bio_block_<?php echo $entry->getId(); ?>").fadeIn();
+					jQuery("a#close_bio_link_<?php echo $entry->getId(); ?>").fadeIn();
+					}); return false'>Full Business Description</a>
 				
 				<?php
-					if ($entry->getWebsites())
-					{
-						//echo '<div style="margin-bottom: 10px;" class="websites">';
-						foreach ($entry->getWebsites() as $website)
-						{
-							if ($website->address != null) echo '<a class="url" href="' . $website->address . '" target="_blank">Visit Website</a>' . "\n";
-							break; // Only show the first stored web address
 						}
-						//echo '</div>';
 					}
 				?>
 	        </td>
-			<td align="right" valign="bottom"  style="text-align: right;">
-				
-				<?php if ($entry->getBio() != '') { ?>
-				
-				<a href="#" id="close_bio_link_<?php echo $entry->getId(); ?>" onclick='jQuery("a#close_bio_link_<?php echo $entry->getId(); ?>").live("click", function(e){
-				jQuery("a#close_bio_link_<?php echo $entry->getId(); ?>").hide();
-				jQuery("#bio_block_<?php echo $entry->getId(); ?>").fadeOut();
-				jQuery("a#bio_link_<?php echo $entry->getId(); ?>").fadeIn();
-				}); return false' style="display: none;">Close Description</a>
-				
-				<a href="#" id="bio_link_<?php echo $entry->getId(); ?>" onclick='jQuery("a#bio_link_<?php echo $entry->getId(); ?>").live("click", function(e){
-				jQuery("a#bio_link_<?php echo $entry->getId(); ?>").hide();
-				jQuery("#bio_block_<?php echo $entry->getId(); ?>").fadeIn();
-				jQuery("a#close_bio_link_<?php echo $entry->getId(); ?>").fadeIn();
-				}); return false'>Business Description</a>
-				
-				<?php } ?>
-				<?php if ($entry->getBio() != '' && $data != '') echo ' | '; ?>
-				
-				<?php if ($data != '') { ?>
-				
-				<a href="#" id="close_map_link_<?php echo $entry->getId(); ?>" class="close_map_link" onclick='jQuery("a#close_map_link_<?php echo $entry->getId(); ?>").live("click", function(e){
-				var divH = jQuery("#entry_id_<?php echo $entry->getId(); ?>").height() - 400;
-				jQuery("#map_<?php echo $entry->getId(); ?>").fadeOut();
-				//jQuery("#entry_id_<?php echo $entry->getId(); ?>").animate({height: divH}, 500);
-				jQuery("a#close_map_link_<?php echo $entry->getId(); ?>").hide();
-				jQuery("a#map_link_<?php echo $entry->getId(); ?>").fadeIn();
-				jQuery("#map_<?php echo $entry->getId(); ?>").remove();
-				}); return false' style="display: none;">Close Map</a>
-				
-				<a href="#" id="map_link_<?php echo $entry->getId(); ?>" class="open_map_link" onclick='jQuery("a#map_link_<?php echo $entry->getId(); ?>").live("click", function(e){
-        		var $this = jQuery(this);
-				var data = $this.attr("data");
-				var divH = jQuery("#entry_id_<?php echo $entry->getId(); ?>").height() + 400;
-				//jQuery("#entry_id_<?php echo $entry->getId(); ?>").animate({height: divH}, 1000);
-				jQuery(data).appendTo(jQuery("#entry_id_<?php echo $entry->getId(); ?>"));
-				jQuery("#map_<?php echo $entry->getId(); ?>").fadeIn(2000);
-				jQuery("a#map_link_<?php echo $entry->getId(); ?>").hide();
-				jQuery("a#close_map_link_<?php echo $entry->getId(); ?>").fadeIn();
-				}); return false' data='<?php echo $data; ?>'>Show Map</a>
-				
-				<?php echo ' | '; ?>
-				
-				<?php } ?>
-				
-				<?php echo $entry->returnToTopAnchor() ?>
-	        </td>
 	    </tr>
+		
+		<?php
+			if ($entry->getBio() != '')
+			{
+		?>
 		
 		<tr>
 			<td colspan="2">
 				<?php 
-					if ($entry->getBio() != '')
-					{
-						echo '<div id="bio_block_' . $entry->getId() . '" style="display: none; margin: 10px 0 0 0;"><strong>Business Description</strong><br />' . $entry->getBioBlock() . '</div>';
-					}
+					echo '<div id="bio_block_' . $entry->getId() . '" style="display: none; margin: 10px 0 0 0;"><strong>Business Description</strong><br />' . $entry->getBioBlock() . '</div>';
 				?>
 			</td>
 		</tr>
+		
+		<?php
+			}
+		?>
 		
 	</table>
 	<?php
