c06c019b61f3eb558ee13693e8f1c347d3a067f0
shazahm1@hotmail.com <shazahm1@hotmail.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-24 15:16:57 +0000

    git-svn-id: https://plugins.svn.wordpress.org/connections/branches/templates@303635 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ sganspach/template.php
@@ -0,0 +1,65 @@
+<div class="cn-entry">
+<table width="100%" border="0px" cellspacing="0px" cellpadding="0px">
+    <tr>
+        <td align="left" width="50%" valign="top">
+        	<div style="clear:both; margin: 0 5px;">
+				<div style="margin-bottom: 10px;">
+					<h3><span style="text-transform: capitalize;"><?php echo $entry->getFullFirstLastNameBlock() ?></span></h3>
+					
+					<?php echo $entry->getTitleBlock() ?>
+					<?php echo $entry->getOrgUnitBlock() ?>
+				</div>
+				<?php echo $entry->getCardImage() ?>
+			</div>
+			
+        </td>
+        <td align="right" valign="top">
+        	<div style="clear:both; margin: 5px 5px;">
+				<?php
+				if ($entry->getAddresses())
+				{
+					foreach ($entry->getAddresses() as $address)
+					{
+						echo '<div class="adr">' . "\n";
+							if ($address->line_one != NULL) echo '<span class="street-address">' . $address->line_one . '</span>' . "\n";
+							if ($address->line_two != NULL) echo '<span class="extended-address">' . $address->line_two . '</span>' . "\n";
+							if ($address->zipcode != NULL) echo '<span class="postal-code">' . $address->zipcode . '</span>&nbsp';
+							if ($address->city != NULL) echo '<span class="locality">' . $address->city . '</span>' . "\n";
+							echo $entry->gethCardAdrType($address->type);
+						echo '</div>' . "\n\n";
+																	
+					}
+				}
+				?>
+				
+	        	<?php echo $entry->getPhoneNumberBlock() ?>
+				
+				<?php
+				if ($entry->getEmailAddresses())
+				{
+					echo '<div class="email-address-block">' . "\n";
+					foreach ($entry->getEmailAddresses() as $email)
+					{
+						//Type for hCard compatibility. Hidden.
+						if ($email->address != NULL) echo '<span class="email"><strong>Email:</strong> <span class="type" style="display: none;">INTERNET</span><a class="value" href="mailto:' . $email->address . '">' . $email->address . '</a></span>' . "\n";
+					}
+					echo '</div>' . "\n";
+				}
+				?>
+				
+				<?php
+				if ($entry->getBirthday()) echo '<span class="vevent"><span class="birthday"><strong>Geburtstag:</strong> <abbr class="dtstart" title="' . $entry->getBirthday('Ymd') .'">' . $entry->getBirthday('d.m') . '</abbr></span>' .
+										 '<span class="bday" style="display:none">' . $entry->getBirthday('Y-m-d') . '</span>' .
+										 '<span class="summary" style="display:none">Birthday - ' . $entry->getFullFirstLastName() . '</span> <span class="uid" style="display:none">' . $entry->getBirthday('YmdHis') . '</span> </span><br />' . "\n";
+				?>
+			</div>
+        </td>
+    </tr>
+    
+    <tr>
+        <td align="right" colspan="2" valign="bottom">
+			<?php echo $entry->returnToTopAnchor() ?>
+        </td>
+    </tr>
+</table>
+</div>
