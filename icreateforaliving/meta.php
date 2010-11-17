5dcf49f49d391621159aaf6ebfa4047829bab068
shazahm1@hotmail.com <shazahm1@hotmail.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-17 00:26:37 +0000

    git-svn-id: https://plugins.svn.wordpress.org/connections/branches/templates@312292 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- icreateforaliving/meta.php
+++
@@ -1,7 +0,0 @@
-<?php
-$template->name = 'icreateforaliving';
-$template->uri = 'connections-pro.com';
-$template->version = '1.0';
-$template->author = 'Steven A. Zahm';
-$template->description = 'Custom template developed for Nick James featuring a category drop down filter.';
-?>
