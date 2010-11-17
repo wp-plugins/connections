fccb045075789447db3d8d59341e4f512893ec7c
shazahm1@hotmail.com <shazahm1@hotmail.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-17 00:25:24 +0000

    git-svn-id: https://plugins.svn.wordpress.org/connections/branches/templates@312286 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- beautyofottawa-premium/meta.php
+++
@@ -1,9 +0,0 @@
-<?php
-$template->name = 'Beauty of Ottwa - Premium';
-$template->uri = 'connections-pro.com';
-$template->version = '1.0';
-$template->author = 'Steven A. Zahm';
-$template->description = 'Custom template built for James Lee';
-$template->legacy = TRUE;
-$template->type = 'all';
-?>
