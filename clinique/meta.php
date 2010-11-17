812ca2363c9785135ea1b4f0036e79e7a830c32f
shazahm1@hotmail.com <shazahm1@hotmail.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-17 00:25:46 +0000

    git-svn-id: https://plugins.svn.wordpress.org/connections/branches/templates@312287 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- clinique/meta.php
+++
@@ -1,7 +0,0 @@
-<?php
-$template->name = 'Clinique';
-$template->uri = 'connections-pro.com';
-$template->version = '1.0';
-$template->author = 'Steven A. Zahm';
-$template->description = 'This is a custom template developed for Ludovic ISMAEL.';
-?>
