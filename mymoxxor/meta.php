8c3558cff5299c6ce23fa51fb48de19e6bace9b7
shazahm1@hotmail.com <shazahm1@hotmail.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-17 00:28:32 +0000

    git-svn-id: https://plugins.svn.wordpress.org/connections/branches/templates@312298 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mymoxxor/meta.php
+++
@@ -1,9 +0,0 @@
-<?php
-$template->name = 'mymoxxor';
-$template->uri = 'connections-pro.com';
-$template->version = '1.0';
-$template->author = 'James Turner';
-$template->description = 'MoxxMer Template.';
-$template->legacy = TRUE;
-$template->type = 'all';
-?> 
