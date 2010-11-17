6ad2c812a9f41ed5c261631a3f20416a33c9d8f9
shazahm1@hotmail.com <shazahm1@hotmail.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-17 00:26:21 +0000

    git-svn-id: https://plugins.svn.wordpress.org/connections/branches/templates@312291 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- grandview/meta.php
+++
@@ -1,9 +0,0 @@
-<?php
-$template->name = 'Grandview Medical Center';
-$template->uri = 'connections-pro.com';
-$template->version = '1.0';
-$template->author = 'Steven A. Zahm';
-$template->description = 'This is a custom template developed for Ben Arrowood featuring search and a category drop down limited by the current category parent.';
-$template->legacy = TRUE;
-$template->type = 'all';
-?>
