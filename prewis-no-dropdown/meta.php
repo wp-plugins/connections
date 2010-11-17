fa3a5ec436d431f7fb6f77d756f02fb33461b90a
shazahm1@hotmail.com <shazahm1@hotmail.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-17 00:29:28 +0000

    git-svn-id: https://plugins.svn.wordpress.org/connections/branches/templates@312301 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- prewis-no-dropdown/meta.php
+++
@@ -1,7 +0,0 @@
-<?php
-$template->name = 'Prepare Wisconsin - No Category Drop Down';
-$template->uri = 'connections-pro.com';
-$template->version = '1.0';
-$template->author = 'Steven A. Zahm';
-$template->description = 'This is a custom template developed for Adam Blust featuring a category drop down limited by the current category parent.';
-?>
