e232c3dd121e4a941de89b66a2a13ced37f87597
shazahm1@hotmail.com <shazahm1@hotmail.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-17 00:29:48 +0000

    git-svn-id: https://plugins.svn.wordpress.org/connections/branches/templates@312302 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sganspach/meta.php
+++
@@ -1,7 +0,0 @@
-<?php
-$template->name = 'SGAnspach';
-$template->uri = 'connections-pro.com';
-$template->version = '1.0';
-$template->author = 'Steven A. Zahm';
-$template->description = 'This is a variation of the default template which is formatted using a table with some text translated to Dutch. This template is recommended when compatibility with Internet Explorer 6 is required.';
-?>
