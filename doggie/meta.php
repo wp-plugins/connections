0196eb70a2cfb2df7f33f6dae9ec58535df462ff
shazahm1@hotmail.com <shazahm1@hotmail.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-17 00:23:21 +0000

    git-svn-id: https://plugins.svn.wordpress.org/connections/branches/templates@312283 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- doggie/meta.php
+++
@@ -1,9 +0,0 @@
-<?php
-$template->name = 'Doggie';
-$template->uri = 'connections-pro.com';
-$template->version = '1.0';
-$template->author = 'Steven A. Zahm';
-$template->description = 'Custom template built for Matthew May with Google Maps [Query based] and Bio field excerpt.';
-$template->legacy = TRUE;
-$template->type = 'all';
-?>
