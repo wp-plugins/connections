8a4557a98413cf6e29b5075b66f1449c07d51d16
shazahm1@hotmail.com <shazahm1@hotmail.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-17 00:27:45 +0000

    git-svn-id: https://plugins.svn.wordpress.org/connections/branches/templates@312295 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lakeontariounited/meta.php
+++
@@ -1,9 +0,0 @@
-<?php
-$template->name = 'Lake Ontario United';
-$template->uri = 'connections-pro.com';
-$template->version = '1.0';
-$template->author = 'Steven A. Zahm';
-$template->description = 'Custom template built for Chad Lapa';
-$template->legacy = TRUE;
-$template->type = 'all';
-?>
