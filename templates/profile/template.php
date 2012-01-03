bf6f58e84dfca001df26d8693f32654b84c0029b
shazahm1@hotmail.com <shazahm1@hotmail.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-03 00:05:55 +0000

    git-svn-id: https://plugins.svn.wordpress.org/connections/tags/0.7.2.3@483682 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/profile/template.php
+++ templates/profile/template.php
@@ -1,13 +1,13 @@
 <div class="cn-entry" style="-moz-border-radius:4px; background-color:#FFFFFF; border:1px solid #E3E3E3; color: #000000; margin:8px 0px; padding:6px; position: relative;">
 	<div>
-		<span style="float: left; margin-right: 10px;"><?php echo $entry->getImage( array( 'preset' => 'profile' ) ) ?></span>
+		<span style="float: left; margin-right: 10px;"><?php $entry->getImage( array( 'preset' => 'profile' ) ); ?></span>
 
 		<div style="margin-left: 10px;">
-			<span style="font-size:larger;font-variant: small-caps"><strong><?php echo $entry->getName() ?></strong></span>
+			<span style="font-size:larger;font-variant: small-caps"><strong><?php $entry->getNameBlock(); ?></strong></span>
 			<div style="margin-bottom: 20px;">
-				<?php $entry->getOrgUnitBlock() ?>
+				<?php $entry->getOrgUnitBlock(); ?>
 			</div>
-			<?php $entry->getBioBlock() ?>
+			<?php echo $entry->getBioBlock(); ?>
 		</div>
 			
 	</div>
