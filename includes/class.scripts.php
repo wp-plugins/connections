98e0304545019f96535bb24adc24668c7814833d
shazahm1@hotmail.com <shazahm1@hotmail.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-28 20:14:43 +0000

    Committing version 0.8
    git-svn-id: https://plugins.svn.wordpress.org/connections/trunk@904337 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/class.scripts.php
+++ includes/class.scripts.php
@@ -42,11 +42,31 @@ class cnScript {
 	 */
 	public static function init() {
 
+		// This filter will add the minified CSS and JS to the search paths
+		// if SCRIPT_DEBUG is not defined or set to FALSE.
+		add_filter( 'cn_locate_file_names', array( __CLASS__, 'minifiedFileNames' ), 10, 5 );
+
 		// Register the CSS JavaScript libraries.
-		add_action( 'init', array( 'cnScript', 'registerScripts' ) );
-		add_action( 'init', array( 'cnScript', 'registerCSS' ) );
+		// NOTE: Seems the `wp` action hook is not fired in the admin or at least not fired
+		// when the CODEX says it is in the admin. So the actions will have to be hooked.
+		// based on if we're in the admin or not.
+		// @see http://codex.wordpress.org/Plugin_API/Action_Reference
+		//
+		// This is required because cnScript depends on cnLocate which depends on get_query_var()
+		// in the frontend but is not needed or available in the admin.
+		if ( is_admin() ) {
+
+			add_action( 'admin_init', array( 'cnScript', 'registerScripts' ) );
+			add_action( 'admin_init', array( 'cnScript', 'registerCSS' ) );
+
+		} else {
+
+			add_action( 'wp', array( 'cnScript', 'registerScripts' ) );
+			add_action( 'wp', array( 'cnScript', 'registerCSS' ) );
+		}
 
 		// Enqueue the frontend scripts and CSS.
+		// add_action( 'wp', array( __CLASS__, 'enqueue' ) );
 		add_action( 'wp_enqueue_scripts', array( 'cnScript', 'enqueueScripts' ) );
 		add_action( 'wp_enqueue_scripts', array( 'cnScript', 'enqueueStyles' ) );
 
@@ -105,8 +125,8 @@ class cnScript {
 		// Disble this for now, Elegant Theme uses the same registration name in the admin which causes errors.
 		// wp_register_script('jquery-spin', CN_URL . 'js/jquery.spin.js', array('jquery'), '1.2.5', $connections->options->getJavaScriptFooter() );
 
-		wp_register_script( 'jquery-chosen-min', CN_URL . "assets/js/jquery.chosen$min.js", array( 'jquery' ), '0.9.11', $connections->options->getJavaScriptFooter() );
-		wp_register_script( 'jquery-validate' , CN_URL . "assets/js/jquery.validate$min.js", array( 'jquery', 'jquery-form' ) , '1.9.0' , $connections->options->getJavaScriptFooter() );
+		wp_register_script( 'jquery-chosen-min', CN_URL . "vendor/chosen/chosen.jquery$min.js", array( 'jquery' ), '1.1.0', $connections->options->getJavaScriptFooter() );
+		wp_register_script( 'jquery-validate' , CN_URL . "vendor/validation/jquery.validate$min.js", array( 'jquery', 'jquery-form' ) , '1.11.1' , $connections->options->getJavaScriptFooter() );
 	}
 
 	/**
@@ -119,18 +139,54 @@ class cnScript {
 	 */
 	public static function registerCSS() {
 
+		// Add a filter so cnLocate will search the plugins CSS folder.
+		add_filter( 'cn_locate_file_paths', array( __CLASS__, 'coreCSSPath' ) );
+
 		// If SCRIPT_DEBUG is set and TRUE load the non-minified CSS files, otherwise, load the minified files.
 		$min = defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ? '' : '.min';
 
 		if ( is_admin() ) {
+
 			wp_register_style( 'cn-admin', CN_URL . "assets/css/cn-admin$min.css", array(), CN_CURRENT_VERSION );
 			wp_register_style( 'cn-admin-jquery-ui', CN_URL . 'assets/css/jquery-ui-' . ( 'classic' == get_user_option( 'admin_color' ) ? 'classic' : 'fresh' ) . "$min.css", array(), CN_CURRENT_VERSION );
+
 		} else {
-			wp_register_style( 'connections-user', CN_URL . "assets/css/cn-user$min.css", array(), CN_CURRENT_VERSION );
-			wp_register_style( 'connections-qtip', CN_URL . "assets/css/jquery.qtip$min.css", array(), '2.0.1' );
+
+			if ( cnSettingsAPI::get( 'connections', 'compatibility', 'css' ) ) {
+
+				// This will locate the CSS file to be enqueued.
+				$coreCSS = cnLocate::file( cnLocate::fileNames( 'cn-user', NULL, NULL, 'css' ), 'url' );
+				// var_dump($coreCSS);
+
+				// Registering the CSS with 'connections-user' for legacy support. Remove this at some point. 04/01/2014
+				wp_register_style( 'connections-user', $coreCSS, array(), CN_CURRENT_VERSION );
+
+				wp_register_style( 'cn-public', $coreCSS, array(), CN_CURRENT_VERSION );
+
+			}
+
+			// This will locate the custom CSS file to be enqueued.
+			$customCSS = cnLocate::file( cnLocate::fileNames( 'cn-custom', NULL, NULL, 'css' ), 'url' );
+			// var_dump($customCSS);
+
+			// If a custom CSS file was found, lets register it.
+			if ( $customCSS ) {
+
+				// Check to see if the core CSS file was registered since it can be disabled.
+				// Add it to the $required array to be used when registering the custom CSS file.
+				$required = wp_style_is( 'cn-public', 'registered' ) ? array( 'cn-public' ) : array();
+
+				wp_register_style( 'cn-public-custom', $customCSS, $required, CN_CURRENT_VERSION );
+			}
+
 		}
 
-		wp_register_style( 'connections-chosen', CN_URL . "assets/css/chosen$min.css", array(), '0.9.11' );
+		wp_register_style( 'cn-qtip', CN_URL . "assets/css/jquery.qtip$min.css", array(), '2.0.1' );
+		wp_register_style( 'cn-chosen', CN_URL . "vendor/chosen/chosen$min.css", array(), '1.1.0' );
+		wp_register_style( 'cn-font-awesome', CN_URL . "vendor/font-awesome/css/font-awesome$min.css", array(), '4.0.3' );
+
+		// Remove the filter that adds the core CSS path to cnLocate.
+		remove_filter( 'cn_locate_file_paths', array( __CLASS__, 'coreCSSPath' ) );
 	}
 
 	/**
@@ -199,6 +255,27 @@ class cnScript {
 			wp_enqueue_script( 'postbox' );
 			wp_enqueue_script( 'cn-widget' );
 		}
+	}
+
+	public static function enqueue() {
+		global $wp_query;
+
+		$posts   = $wp_query->posts;
+		$pattern = get_shortcode_regex();
+
+		foreach ( $posts as $post ) {
+
+			if ( preg_match_all( '/'. $pattern .'/s', $post->post_content, $matches )
+				&& array_key_exists( 2, $matches )
+				&& in_array( 'connections', $matches[2] ) )
+			{
+				add_action( 'wp_enqueue_scripts', array( __CLASS__, 'enqueueScripts' ) );
+				add_action( 'wp_enqueue_scripts', array( __CLASS__, 'enqueueStyles' ) );
+
+				break;
+			}
+		}
+
 	}
 
 	/**
@@ -283,6 +360,7 @@ class cnScript {
 		if ( in_array( $pageHook, get_object_vars( $connections->pageHook ) ) ) {
 			wp_enqueue_style( 'cn-admin' );
 			wp_enqueue_style( 'cn-admin-jquery-ui' );
+			wp_enqueue_style( 'cn-font-awesome' );
 		}
 
 		// Load the WordPress widgets styles only on these pages.
@@ -301,14 +379,117 @@ class cnScript {
 	 * Enqueues the Connections CSS on the frontend.
 	 *
 	 * @access private
-	 * @since 0.7.3.2
-	 * @uses wp_enqueue_style()
+	 * @since  0.7.3.2
+	 * @static
+	 * @uses   wp_enqueue_style()
+	 *
 	 * @return void
 	 */
 	public static function enqueueStyles() {
-		wp_enqueue_style( 'connections-user' );
-		wp_enqueue_style( 'connections-chosen' );
-		// wp_enqueue_style( 'connections-qtip' );
+
+		if ( cnSettingsAPI::get( 'connections', 'compatibility', 'css' ) ) {
+
+			wp_enqueue_style( 'cn-public' );
+			wp_enqueue_style( 'cn-chosen' );
+
+			// If the custom CSS file was registered, lets enqueue it.
+			if ( wp_style_is( 'cn-public-custom', 'registered' ) ) {
+
+				wp_enqueue_style( 'cn-public-custom' );
+			}
+
+		}
+
+	}
+
+	/**
+	 * This is the callback function that will add the minified CSS and JS
+	 * file names to the file name array.
+	 *
+	 * The minified file names will only be added if SCRIPT_DEBUG is defined
+	 * and set to true.
+	 *
+	 * @access private
+	 * @since  0.8
+	 * @static
+	 * @see     cnLocate::fileNames()
+	 * @param  array  $files An indexed array of file names to search for.
+	 * @param  string $base The base file name. Passed via filter from  cnLocate::fileNames().
+	 * @param  string $name The template part name. Passed via filter from  cnLocate::fileNames().
+	 * @param  string $slug The template part slug. Passed via filter from  cnLocate::fileNames().
+	 * @param  string $ext  The template file name extension. Passed via filter from  cnLocate::fileNames().
+	 *
+	 * @return array        An indexed array of file names to search for.
+	 */
+	public static function minifiedFileNames( $files, $base, $name, $slug, $ext ) {
+
+		// If SCRIPT_DEBUG is set and TRUE the minified file names
+		// do not need added to the $files name array.
+		if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) return $files;
+
+		if ( $ext == 'css' || $ext == 'js' ) {
+
+			$i = 0;
+
+			foreach ( $files as $key => $fileName ) {
+
+				// Create the minified file name.
+				$position = strrpos( $fileName, '.' );
+				$minified = substr( $fileName, 0, $position ) . '.min' . substr( $fileName, $position );
+
+				// Insert the minified file name into the array.
+				array_splice( $files, $i, 0, $minified );
+
+				// Increment the insert position. Adding `2` to take into account the updated insert postion
+				// due to an item being inserted into the array.
+				$i = $i + 2;
+			}
+
+		}
+
+		return $files;
+	}
+
+	/**
+	 * This a callback for the filter `cn_locate_file_paths` which adds
+	 * the core plugin CSS file path to the file paths which are searched
+	 * when locating a CSS file.
+	 *
+	 * @access private
+	 * @since  0.8
+	 * @static
+	 * @see    registerCSS()
+	 * @see    cnLocate::filePaths()
+	 * @param  array  $paths An index array containing the file paths to be searched.
+	 * @return array
+	 */
+	public static function coreCSSPath( $paths ) {
+
+		$paths[9999] = CN_PATH . 'assets/css/';
+
+		return $paths;
+	}
+
+	/**
+	 * This a callback for the filter `cn_locate_file_paths` which adds
+	 * the core plugin JS file path to the file paths which are searched
+	 * when locating a JS file.
+	 *
+	 * @access private
+	 * @since  0.8
+	 * @static
+	 * @see    registerCSS()
+	 * @param  array  $paths An index array containing the file paths to be searched.
+	 * @return array
+	 */
+	public static function coreJSPath( $paths ) {
+
+		$paths[9999] = CN_PATH . 'assets/js/';
+
+		return $paths;
 	}
 
 }
+
+// Init the class.
+cnScript::init();
